6126eb5eb043cd355bc3ec0e23f9448ed1800d5f
silvi <silvisnigdha09@gmail.com>
2023-02-09 11:46:17 -0500

    abc

--- src/com/upskill/java_3/Constructor.java
+++ src/com/upskill/java_3/Constructor.java
@@ -7,12 +7,12 @@ package com.upskill.java_3;
  */
 public class Constructor {
 	
-	String studentName;												// variable declare
+	String studentName;												// variable declare ( string & int is data type studentName & studentAge is variabale)
 	int studentAge;													// variable declare
 	
 	public Constructor(String name, int age) {
-		studentName = name;											// variable initialize
-		studentAge = age;											// variable initialize
+		studentName = name;											// variable initialize (here studentName is variable)
+		studentAge = age;											// variable initialize (here studentAge is variable)
 	}
 	
 	public static void main(String[] args) {
